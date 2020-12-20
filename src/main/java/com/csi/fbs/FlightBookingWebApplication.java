414c426d3ce9027e6c4a59e4bd195602d42334df
unknown <ganeshbidve07@gmail.com>
2020-12-20 17:31:49 +0530

    first commit

---
+++ src/main/java/com/csi/fbs/FlightBookingWebApplication.java
@@ -0,0 +1,13 @@
+package com.csi.fbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csi/fbs/FlightBookingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csi.fbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
